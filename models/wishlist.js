77c0b9bf6ffe702d66822e44a2653f06187d508b
Nattapat Sittichai <frank_franky085@hotmail.com>
2021-06-15 19:06:57 +0700

    ed
    ed

--- models/wishlist.js
+++ models/wishlist.js
@@ -4,12 +4,7 @@ var Schema = mongoose.Schema;
 var schema = new Schema({
     user: {type: Schema.Types.ObjectId, ref: 'User'},
     productid: {type: Schema.Types.ObjectId, ref: 'Product'},
-    // cart: {type: Object, required: true}
-    // imagePath: {type: String, required: true},
     title: {type: String, required: true},
-    // description: {type: String, required: true},
-    // selldescription: {type: String, required: true},
-    // type: {type: String, required: true},
     tag: {type: String, required: true},
     price: {type: Number, required: true}
 });
